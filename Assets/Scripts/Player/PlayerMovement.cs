aa157f5d8ee5a122209015f5399dc59ae57a2c40
James Johnstone <jjohnstone2520@gmail.com>
2020-05-01 13:32:47 +1200

    ~Added player movement ~Added player jumping (w/ any up or jump buttons) ~ Added aiming at mouse position

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public CharacterController2D controller;
+
+    public float moveSpeed = 40.0f;
+
+    private float horizontalMovement = 0.0f;
+    private bool isJumping = false;
+
+    private void Update()
     {
-        
+        horizontalMovement = Input.GetAxisRaw("Horizontal") * moveSpeed;
+
+        if (Input.GetAxisRaw("Vertical") > 0 || Input.GetButton("Jump"))
+        {
+            isJumping = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        controller.Move(horizontalMovement * Time.fixedDeltaTime, false, isJumping);
+        isJumping = false;
     }
 }
